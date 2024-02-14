f19d67549e6cd96ed04c6b241e2179a308343106
el <myemail.ru>
2024-02-15 00:26:29 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhc_iKcA7GFYcM4ByXfg0I0TJgzI5tckw8ug&usqp=CAU"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://a.cdn-hotels.com/gdcs/production49/d1115/384d7c82-7494-45d7-8eb8-b2d46be78a33.jpg?impolicy=fcrop&w=800&h=533&q=medium"
+          alt="logo"
+        />
+      </div>
+      <div>ava + discription</div>
+      <div>
+        my posts
+        <div>new post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
