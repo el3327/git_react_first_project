820b86bc253bddbe08fef1d2a95ec89b060d0236
spring <potter@mail.ru>
2024-07-12 15:13:36 +0600

    props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,11 +10,11 @@ const MyPosts = () => {
         <textarea></textarea>
         <button>Add post</button>
       </div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post message="Hey, why nobody love me?" likesCount="15" />
+      <Post message="It`s our program!Hey!" likesCount="20" />
+      <Post message="Hello! I love you!" likesCount="125" />
+      <Post message="Where are you?" likesCount="44" />
+      <Post message="Hello!Hello!Hello!" likesCount="325" />
     </div>
   );
 };
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div>
       <div className={s.item}>
         <img
           src="https://static.independent.co.uk/2022/08/22/15/newFile-2.jpg?width=1200&height=1200&fit=crop"
-          alt=""
+          alt="picture"
         />
-        post 1
+        {props.message}
         <div>
-          <span>like</span>
+          <span>like </span> {props.likesCount}
         </div>
       </div>
     </div>
